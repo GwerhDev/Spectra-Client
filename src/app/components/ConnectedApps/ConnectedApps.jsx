abae7ec872a09edfd1c5f2cb1bf4e78ff89dbd99
GwerhDev <gwerh.dev@gmail.com>
2025-03-02 12:08:33 -0300

    using old la ruina tv for new streaming app spectra

--- src/app/components/ConnectedApps/ConnectedApps.jsx
+++ src/app/components/ConnectedApps/ConnectedApps.jsx
@@ -3,13 +3,12 @@ import { useSelector } from 'react-redux';
 import { getUserToken } from '../../../middlewares/helpers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload, faLayerGroup, faMusic, faStore } from '@fortawesome/free-solid-svg-icons';
-import { faSoundcloud } from '@fortawesome/free-brands-svg-icons';
 
 export const ConnectedApps = (props) => {
   const { width } = props || {};
   const currentUser = useSelector((state) => state.currentUser);
-  const urlMerch = 'https://havenstore.nhexa.cl/';
   const urlNhexaHub = 'https://download.nhexa.cl/?product=nhexahub-desktop';
+  const urlMerch = currentUser ? 'https://havenstore.nhexa.cl/auth/' + getUserToken() : 'https://havenstore.nhexa.cl/';
   const urlSpellcast = currentUser ? 'https://spellcast.nhexa.cl/auth/' + getUserToken() : 'https://spellcast.nhexa.cl/';
 
   return (
