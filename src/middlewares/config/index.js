3ea2ab4ff6d90408b7b163637c38200fa6214ea1
GwerhDev <gwerh.dev@gmail.com>
2025-04-15 12:36:56 -0400

    updating api url

--- src/middlewares/config/index.js
+++ src/middlewares/config/index.js
@@ -3,7 +3,7 @@ import { DEVELOPMENT } from "../misc";
 
 export const URL_CLIENT = environment === DEVELOPMENT ? "http://localhost:5174":"https://spectra.nhexa.cl";
 export const URL_STREAMBY = environment === DEVELOPMENT ? "http://localhost:1312":"https://streamby-api.vercel.app";
-export const URL_NHEXA_API = environment === DEVELOPMENT ? "http://localhost:8080":"https://nhexa-api.fly.dev";
+export const URL_NHEXA_API = environment === DEVELOPMENT ? "http://localhost:8080":"https://api.nhexa.cl";
 export const URL_SPECTRA_API = environment === DEVELOPMENT ? "http://localhost:8081":"https://spectra-nhexa-api.fly.dev";
 
 export const URL_NHEXA_ACCOUNTS = environment === DEVELOPMENT ? "http://localhost:5173" : "https://accounts.nhexa.cl";
